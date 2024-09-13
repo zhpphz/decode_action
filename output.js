63eb72f8aafa29e8fac28b636f2563e024a81a65
action <action@github.com>
2024-09-13 02:34:54 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Thu Sep 12 2024 16:49:03 GMT+0000 (Coordinated Universal Time)
+//Fri Sep 13 2024 02:34:54 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
 const _0x4c672b = _0x4a9430("中国联通"),
